05aa8dcdbaf11dc61a5f50c7c40c1ead53b8a8e6
DESKTOP-T69QAHC\WINDOWS 10 <jaimemartin.dolor@benilde.edu.ph>
2021-08-23 18:34:16 +0800

    Tried Sample Project

---
+++ Sample Project/src/com/dolorjaime/Main.java
@@ -0,0 +1,14 @@
+package com.dolorjaime;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Print Hello World
+	    System.out.println("Hello World!");
+
+	    //Print Age
+	    int age = 30;
+	    System.out.println(age);
+    }
+}
